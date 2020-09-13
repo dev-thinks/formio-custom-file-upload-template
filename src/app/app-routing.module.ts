1784cdab9404785ffab2c1acedcbfebe908609e5
dev-thinks <kaykay@programmer.net>
2020-09-13 13:00:22 -0400

    I don't know what the hell I was thinking.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {PrimeFileUploadComponent} from './prime-file-upload/prime-file-upload.component';
 import {FormGenComponent} from './form-gen/form-gen.component';
+import {FormRenderComponent} from './form-render/form-render.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'builder',
     component: FormGenComponent
+  },
+  {
+    path: 'renderer',
+    component: FormRenderComponent
   },
   {path: '', redirectTo: 'primeupload', pathMatch: 'full'},
   {path: '**', redirectTo: 'primeupload'}
--- src/app/form-render/form-render.component.ts
+++ src/app/form-render/form-render.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, ViewChild} from '@angular/core';
+import {FormioComponent} from 'angular-formio';
+import * as formJson from './formschema.json';
+
+declare var jQuery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-form-render',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormRenderComponent implements OnInit {
 
-  constructor() { }
+  form: any;
+  data = {};
+  rendered = false;
+  currentForm: any;
+  @ViewChild('frmBuilder') frmBuilder?: any;
+  refreshForm: any;
+  @ViewChild(FormioComponent) formioComponent: FormioComponent;
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.form = (formJson as any).default;
+
+    this.refreshForm = new EventEmitter();
+  }
+
+  onReady(event) {
+    console.log('Form ready!');
+    this.currentForm = event.formio;
+  }
+
+  onRendered() {
+    console.log('Form rendered!');
+    this.rendered = true;
+  }
+
+  onChange(event) {
+    console.log('Submission changed!', event);
+    if (event.data) {
+      this.data = event.data;
+    }
+  }
+
+  onSubmit(event) {
+    console.log(this.frmBuilder);
+
+    this.formioComponent.formio.disabled = true;
+
+    console.log(this.frmBuilder);
+
+    console.log('form submitted!')
   }
 
 }

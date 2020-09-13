70bc55d319913bf347ed3f08610aa802b297b552
dev-thinks <kaykay@programmer.net>
2020-09-13 12:44:30 -0400

    Git wants e to commit, I want to sleep. Take me sweet void.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {FileUploadModule} from 'primeng/fileupload';
 import {CardModule} from 'primeng/card';
-import { PrimeFileUploadComponent } from './prime-file-upload/prime-file-upload.component';
-import { FormGenComponent } from './form-gen/form-gen.component';
+import {PrimeFileUploadComponent} from './prime-file-upload/prime-file-upload.component';
+import {FormGenComponent} from './form-gen/form-gen.component';
 import {FormioModule} from 'angular-formio';
-import { FormRenderComponent } from './form-render/form-render.component';
+import {FormRenderComponent} from './form-render/form-render.component';
 
 @NgModule({
   declarations: [
